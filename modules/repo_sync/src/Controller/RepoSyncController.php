d4b42427d794e17bb4001ba6abe1119940ce48f8
Lékó Bertalan <bertalan.leko@pronovix.com>
2018-06-05 15:58:20 +0200

    Changes to controller and create form
    Removed redundant dependency injection of the messenger service from the
    controller and the create form. Changed how the rows are built in the
    controller. Removed repository type select option from the create form.

--- modules/repo_sync/src/Controller/RepoSyncController.php
+++ modules/repo_sync/src/Controller/RepoSyncController.php
@@ -3,40 +3,13 @@
 namespace Drupal\devportal_repo_sync\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\Core\Url;
 use Drupal\devportal_repo_sync\Service\Client;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class RepoSyncController.
  */
 class RepoSyncController extends ControllerBase {
-
-  /**
-   * Drupal\Core\Messenger\MessengerInterface definition.
-   *
-   * @var \Drupal\Core\Messenger\MessengerInterface
-   */
-  protected $messenger;
-
-  /**
-   * Constructs a new RepoSyncController object.
-   *
-   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
-   */
-  public function __construct(MessengerInterface $messenger) {
-    $this->messenger = $messenger;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('messenger')
-    );
-  }
 
   /**
    * Creates a table from the imported repositories.
@@ -52,24 +25,31 @@ class RepoSyncController extends ControllerBase {
       $result = $client("GET", "/api/import", NULL);
       $result = json_decode(array_pop($result));
 
-      $links = [];
-      $links['view'] = [
-        'title' => $this->t('View'),
-        'url' => Url::fromRoute('devportal_repo_sync.devportal_repo_sync_config_form'),
-      ];
-      $links['delete'] = [
-        'title' => $this->t('Delete'),
-        'url' => Url::fromRoute('devportal_repo_sync.devportal_repo_sync_create_form'),
-      ];
-      $operations = [
-        'data' => [
-          '#type' => 'operations',
-          '#links' => $links,
-        ]
-      ];
-
       foreach ($result->items as $item) {
-        $rows[] = [$item->Label, $item->ID, $item->Owner, $operations];
+        $rows[] = [
+          $item->Label,
+          $item->ID,
+          $item->Owner,
+          [
+            'data' => [
+              '#type' => 'operations',
+              '#links' => [
+                'view' => [
+                  'title' => $this->t('View'),
+                  'url' => Url::fromRoute('devportal_repo_sync.devportal_repo_sync_config_form'),
+                ],
+                'edit' => [
+                  'title' => $this->t('Edit'),
+                  'url' => Url::fromRoute('devportal_repo_sync.devportal_repo_sync_config_form'),
+                ],
+                'delete' => [
+                  'title' => $this->t('Delete'),
+                  'url' => Url::fromRoute('devportal_repo_sync.devportal_repo_sync_delete_form', ['uuid' => $item->ID]),
+                ],
+              ],
+            ],
+          ],
+        ];
       }
     }
     catch (\Exception $e) {
@@ -80,7 +60,7 @@ class RepoSyncController extends ControllerBase {
       $build = [
         '#type' => 'table',
         '#caption' => $this->t('Repository Synchronization settings overview.'),
-        '#header' => [$this->t('Name'), $this->t('ID'), $this->t('Owner'), $this->t('Operations')],
+        '#header' => [$this->t('Name'), $this->t('UUID'), $this->t('Owner'), $this->t('Operations')],
         '#rows' => $rows,
         '#description' => $this->t('Repository Synchronization settings overview.'),
       ];
--- modules/repo_sync/src/Form/RepoSyncCreateForm.php
+++ modules/repo_sync/src/Form/RepoSyncCreateForm.php
@@ -4,39 +4,12 @@ namespace Drupal\devportal_repo_sync\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\devportal_repo_sync\Service\Client;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class RepoSyncCreateForm.
  */
 class RepoSyncCreateForm extends FormBase {
-
-  /**
-   * Drupal\Core\Messenger\MessengerInterface definition.
-   *
-   * @var \Drupal\Core\Messenger\MessengerInterface
-   */
-  protected $messenger;
-
-  /**
-   * Constructs a new RepoSyncController object.
-   *
-   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
-   */
-  public function __construct(MessengerInterface $messenger) {
-    $this->messenger = $messenger;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('messenger')
-    );
-  }
 
   /**
    * {@inheritdoc}
@@ -55,17 +28,8 @@ class RepoSyncCreateForm extends FormBase {
       '#description' => $this->t('A name for the import project.'),
       '#maxlength' => 64,
       '#size' => 64,
-      '#default_value' => $this->t('Test import'),
+      '#default_value' => '',
       '#weight' => 1,
-    ];
-    $form['repository_type'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Repository Type'),
-      '#description' => $this->t('Select the repository type.'),
-      '#options' => ['git' => $this->t('Git')],
-      '#size' => 1,
-      '#default_value' => 'git',
-      '#weight' => 2,
     ];
     $form['repository_url'] = [
       '#type' => 'textfield',
@@ -73,7 +37,7 @@ class RepoSyncCreateForm extends FormBase {
       '#description' => $this->t('The URL of the repository to import.'),
       '#maxlength' => 128,
       '#size' => 64,
-      '#default_value' => 'https://github.com/tamasd/git-test.git',
+      '#default_value' => '',
       '#weight' => 3,
     ];
     $form['pattern'] = [
@@ -82,7 +46,7 @@ class RepoSyncCreateForm extends FormBase {
       '#description' => $this->t('A pattern to look for in the repository.'),
       '#maxlength' => 64,
       '#size' => 64,
-      '#default_value' => 'docs/',
+      '#default_value' => '',
       '#weight' => 4,
     ];
     $form['reference'] = [
@@ -91,7 +55,7 @@ class RepoSyncCreateForm extends FormBase {
       '#description' => $this->t('Branch reference'),
       '#maxlength' => 64,
       '#size' => 64,
-      '#default_value' => 'refs/heads/master',
+      '#default_value' => '',
       '#weight' => 5,
     ];
     $form['callback'] = [
@@ -100,12 +64,13 @@ class RepoSyncCreateForm extends FormBase {
       '#description' => $this->t('Callback URL'),
       '#maxlength' => 64,
       '#size' => 64,
-      '#default_value' => 'http://storage:8888',
+      '#default_value' => '',
       '#weight' => 6,
     ];
     $form['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Submit'),
+      '#weight' => 7,
     ];
 
     return $form;
@@ -122,6 +87,7 @@ class RepoSyncCreateForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    $form_state->setRedirect('devportal_repo_sync.repo_sync_controller_content');
     $values = $form_state->getValues();
     $config = $this->config('devportal_repo_sync.config');
     $client = new Client($config->get('uuid'), hex2bin($config->get('secret')), "http://service:8000");
@@ -129,7 +95,7 @@ class RepoSyncCreateForm extends FormBase {
     try {
       $client("POST", "/api/import", json_encode([
         "Label" => $values["label"],
-        "RepositoryType" => $values["repository_type"],
+        "RepositoryType" => 'git',
         "RepositoryURL" => $values["repository_url"],
         "Pattern" => $values["pattern"],
         "Reference" => $values["reference"],
